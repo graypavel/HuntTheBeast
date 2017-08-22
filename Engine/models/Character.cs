387466cdb85b8e788c15f877df769e7a139f31f2
graypavel@gmail.com <123SeB7a>
2017-08-22 10:59:50 +0700

    Рефактор

--- Engine/models/Character.cs
+++ Engine/models/Character.cs
@@ -6,7 +6,6 @@ namespace Engine.models
     public abstract class Character
     {
         public Coordinate Position;
-        //public abstract Coordinate GetNextMove(Field field);
 
         protected abstract List<Coordinate> GetAvailableMoves();
 
--- Engine/models/Hunter.cs
+++ Engine/models/Hunter.cs
@@ -6,12 +6,6 @@ namespace Engine.models
 {
     public class Hunter: Character
     {
-        //public override Coordinate GetNextMove(Field field)
-        //{
-        //    //Реализуем, если потребуется автоматизировать охотников
-        //    throw new NotImplementedException();
-        //}
-
         protected override List<Coordinate> GetAvailableMoves()
         {
             //Реализуем, если потребуется автоматизировать охотников
